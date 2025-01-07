deae65b7dfbf47b7bf034009299ff47a3988ed4f
Alfred Yael <alfred.yael16@gmail.com>
2025-01-07 14:33:09 +0100

    TP5 Exercice 1

---
+++ Td4/Ensemble.java
@@ -0,0 +1,8 @@
+Interface Ensemble<Integer>{
+    public void ajouter(Integer t);
+    public void supprimer(Integer t);
+    public boolean contient(Integer t);
+    public boolean estVide();
+    public int cardinalite();
+    public Integer plusPetit();
+}
